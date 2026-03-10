57af42641b0a25f736e5d3f204fd1f219517fe36
Diego Arroyo <diegomejiarroyo11@ciencias.unam.mx>
2026-03-09 20:52:50 -0600

    primercommit

--- src/Practica02.hs
+++ src/Practica02.hs
@@ -1,21 +1,25 @@
 module Practica02 where
 
---Sintaxis de la logica proposicional
-data Prop = Var String | Cons Bool | Not Prop
-            | And Prop Prop | Or Prop Prop
-            | Impl Prop Prop | Syss Prop Prop
-            deriving (Eq)
+-- Sintaxis de la logica proposicional
+data Prop
+  = Var String
+  | Cons Bool
+  | Not Prop
+  | And Prop Prop
+  | Or Prop Prop
+  | Impl Prop Prop
+  | Syss Prop Prop
+  deriving (Eq)
 
-instance Show Prop where 
-                    show (Cons True) = "⊤"
-                    show (Cons False) = "⊥"
-                    show (Var p) = p
-                    show (Not p) = "¬" ++ show p
-                    show (Or p q) = "(" ++ show p ++ " ∨ " ++ show q ++ ")"
-                    show (And p q) = "(" ++ show p ++ " ∧ " ++ show q ++ ")"
-                    show (Impl p q) = "(" ++ show p ++ " → " ++ show q ++ ")"
-                    show (Syss p q) = "(" ++ show p ++ " ↔ " ++ show q ++ ")"
-
+instance Show Prop where
+  show (Cons True) = "⊤"
+  show (Cons False) = "⊥"
+  show (Var v) = v
+  show (Not a) = "¬" ++ show a
+  show (Or a b) = "(" ++ show a ++ " ∨ " ++ show b ++ ")"
+  show (And a b) = "(" ++ show a ++ " ∧ " ++ show b ++ ")"
+  show (Impl a b) = "(" ++ show a ++ " → " ++ show b ++ ")"
+  show (Syss a b) = "(" ++ show a ++ " ↔ " ++ show b ++ ")"
 
 p, q, r, s, t, u :: Prop
 p = Var "p"
@@ -27,42 +31,81 @@ u = Var "u"
 
 type Estado = [String]
 
---EJERCICIOS
+-- EJERCICIOS
 
---Ejercicio 1
+-- Ejercicio 1
 variables :: Prop -> [String]
-variables = undefined
+variables prop = sinRepetidos (go prop)
+  where
+    go (Var v) = [v]
+    go (Cons _) = []
+    go (Not a) = go a
+    go (And a b) = go a ++ go b
+    go (Or a b) = go a ++ go b
+    go (Impl a b) = go a ++ go b
+    go (Syss a b) = go a ++ go b
 
---Ejercicio 2
+-- Ejercicio 2
 interpretacion :: Prop -> Estado -> Bool
-interpretacion = undefined
+interpretacion (Var v) estado = pertenece v estado
+interpretacion (Cons b) _ = b
+interpretacion (Not a) estado = negacion (interpretacion a estado)
+interpretacion (And a b) estado = interpretacion a estado && interpretacion b estado
+interpretacion (Or a b) estado = interpretacion a estado || interpretacion b estado
+interpretacion (Impl a b) estado = negacion (interpretacion a estado) || interpretacion b estado
+interpretacion (Syss a b) estado = interpretacion a estado == interpretacion b estado
 
---Ejercicio 3
+-- Ejercicio 3
 estadosPosibles :: Prop -> [Estado]
-estadosPosibles = undefined
+estadosPosibles prop = conjPotencia (variables prop)
 
---Ejercicio 4
+-- Ejercicio 4
 modelos :: Prop -> [Estado]
 modelos = undefined
 
---Ejercicio 5
+-- Ejercicio 5
 sonEquivalentes :: Prop -> Prop -> Bool
 sonEquivalentes = undefined
 
---Ejercicio 6 
+-- Ejercicio 6
 tautologia :: Prop -> Bool
 tautologia = undefined
 
---Ejercicio 7
+-- Ejercicio 7
 contradiccion :: Prop -> Bool
 contradiccion = undefined
 
---Ejercicio 8
+-- Ejercicio 8
 consecuenciaLogica :: [Prop] -> Prop -> Bool
 consecuenciaLogica = undefined
 
+-- Funciones auxiliares propias (sin librerias externas)
 
---Funcion auxiliar
+-- Conjunto potencia de una lista
 conjPotencia :: [a] -> [[a]]
 conjPotencia [] = [[]]
-conjPotencia (x:xs) = [(x:ys) | ys <- conjPotencia xs] ++ conjPotencia xs
+conjPotencia (x : xs) = [(x : ys) | ys <- conjPotencia xs] ++ conjPotencia xs
+
+-- Elimina repetidos de una lista de Strings (equiv. a Data.List.nub)
+sinRepetidos :: [String] -> [String]
+sinRepetidos [] = []
+sinRepetidos (x : xs)
+  | pertenece x xs = sinRepetidos xs
+  | otherwise = x : sinRepetidos xs
+
+-- Pertenencia en lista de Strings (equiv. a elem)
+pertenece :: String -> [String] -> Bool
+pertenece _ [] = False
+pertenece v (x : xs) = v == x || pertenece v xs
+
+-- Negacion booleana (equiv. a not)
+negacion :: Bool -> Bool
+negacion True = False
+negacion False = True
+
+-- Filtro de lista (equiv. a filter)
+filtra :: (a -> Bool) -> [a] -> [a]
+filtra _ [] = []
+filtra f (x : xs)
+  | f x = x : filtra f xs
+  | otherwise = filtra f xs
